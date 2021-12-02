52498c4d1cf157181ed274b0d75bba7e9d3c2ce8
c-butkus <92456453+c-butkus@users.noreply.github.com>
2021-12-02 13:54:34 -0500

    Update saltwater_intrusion_pde.m

--- saltwater_intrusion_pde.m
+++ saltwater_intrusion_pde.m
@@ -4,7 +4,7 @@ clc
 % Parameters:
 k = 2; %m/d, hydraulic conductivity
 n = 0.2; %effective porosity
-S = 0.1; %storage coefficient
+S = 1; %storage coefficient
 H = 45; %m, thickness of aquifer
 zb = -45; %m, bottom of aquifer
 zt = 0; %m, top of aquifer
@@ -24,41 +24,57 @@ x = 0; %coastline
 h0 = 0.05; %m, head at x=0
 zeta = -2; %m
 
-%% boundary conditions
-Q0 = 0.1; %m2/d, uniform flow towards coast
-zeta0 = -Q0/(k*vs); %boundary condition at the coast obtained with Glover’s formula
-%at
-t = 0;
-Q = -0.05; %m2/d, discharge of the gallery
-%at
-x = 0;
-h0 = 0.05; %m, head of freshwater zone fixed
-zeta0 = -2; %m, interface
+% %% boundary conditions
+% Q0 = 0.1; %m2/d, uniform flow towards coast
+% zeta0 = -Q0/(k*vs); %boundary condition at the coast obtained with Glover’s formula
+% %at
+% t = 0;
+% Q = -0.05; %m2/d, discharge of the gallery
+% %at
+% x = 0;
+% h0 = 0.05; %m, head of freshwater zone fixed
+% zeta0 = -2; %m, interface
 
 %% for equation 1:
 % set up matrix
 m = xf/dx + 1; %space steps
 n = tf/dt + 1; % time steps
 
-M = speye(m,m);
+% template matrix for h
+M1 = speye(m,m);
 for i = 1:m
     for j = 1:m
         if i == j
-            M(i,j) = -2*(T*dt/(dx^2*S))+1;
+            M1(i,j) = -2*(T*dt/(dx^2*S))+1;
         elseif i-1==j
-            M(i,j) = T*dt/(dx^2*S);
+            M1(i,j) = T*dt/(dx^2*S);
         elseif i+1 ==j
-            M(i,j) = T*dt/(dx^2*S);
+            M1(i,j) = T*dt/(dx^2*S);
         end
     end
     
 end 
+%boundary conditions for h
+M1(1,1) = 0.5;
 
-h=h0*ones(m,1); %initial condition: at x=0, h=0.05
-h_all = zeros(m,n);
-h_all(:,1) = 0.5;
 for t = 2:n
     hnew = M*h;
-    h_all(:, t) = hnew';
+    h_all(:, X) = hnew';
     h = hnew;
 end
+
+O = speye(n,n);
+for i = 1:n
+    for j = 1:n
+        if i == j
+            O(i,j) = -2*(tau*dt/(dx^2*n))+1;
+        elseif i-1==j
+            O(i,j) = tau*dt/(dx^2*n);
+        elseif i+1 ==j
+            O(i,j) = tau*dt/(dx^2*n);
+        end
+    end
+    
+end 
+%initial condition at x=0, zeta = -2
+z=z0*ones(1,n)
