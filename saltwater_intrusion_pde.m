d27e90c97826d72325bbb0303c889801e50cdb63
c-butkus <92456453+c-butkus@users.noreply.github.com>
2021-12-02 18:29:57 -0500

    Update saltwater_intrusion_pde.m

--- saltwater_intrusion_pde.m
+++ saltwater_intrusion_pde.m
@@ -13,6 +13,7 @@ W = 1; %m, width of cross section normal to plane of flow
 vs = 0.025; %dimensionless density
 T = k*(zt-zb); %transmissivity of freshwater
 tau = k*vs*(zt-zb); %transmissivity of saltwater
+N = 1; %infiltration of freshwater (dimensionless?) 
 
 %% stepping
 dx  = 10; %m
@@ -24,7 +25,7 @@ x = 0; %coastline
 h0 = 0.05; %m, head at x=0
 zeta = -2; %m
 
-% %% boundary conditions
+% %% boundary conditions???
 % Q0 = 0.1; %m2/d, uniform flow towards coast
 % zeta0 = -Q0/(k*vs); %boundary condition at the coast obtained with Glover’s formula
 % %at
@@ -41,40 +42,34 @@ m = xf/dx + 1; %space steps
 n = tf/dt + 1; % time steps
 
 % template matrix for h
-M1 = speye(m,m);
+M = speye(m,m);
 for i = 1:m
     for j = 1:m
         if i == j
-            M1(i,j) = -2*(T*dt/(dx^2*S))+1;
+            M(i,j) = -2*(T*dt/(dx^2*S))+1;
         elseif i-1==j
-            M1(i,j) = T*dt/(dx^2*S);
+            M(i,j) = T*dt/(dx^2*S);
         elseif i+1 ==j
-            M1(i,j) = T*dt/(dx^2*S);
+            M(i,j) = T*dt/(dx^2*S);
         end
     end
     
 end 
 %boundary conditions for h
-M1(1,1) = 0.5;
+M(1,1) = 0.05;
 
-%S1 = speye(m,m);
-%for i = 1:m
- %   for j = 1:m
-  %      if i == j
-   %         S1(i,j) = -2*(tau*dt/(S*dx^2));
-    %    elseif i-1==j
-     %       S1(i,j) = tau*dt/(S*dx^2);
-      %  elseif i+1 ==j
-       %     S1(i,j) = tau*dt/(S*dx^2);
-        %end
-    %end
-%end 
-
-for t = 2:n
-    hnew = M1*h;
-    h_all(:, X) = hnew';
-    h = hnew;
-end
+S1 = speye(m,m);
+for i = 1:m
+   for j = 1:m
+       if i == j
+           S1(i,j) = -2*(tau*dt/(S*dx^2));
+       elseif i-1==j
+           S1(i,j) = tau*dt/(S*dx^2);
+       elseif i+1 ==j
+           S1(i,j) = tau*dt/(S*dx^2);
+        end
+    end
+end 
 
 O = speye(m,m);
 for i = 1:m
@@ -86,21 +81,45 @@ for i = 1:m
         elseif i+1 ==j
             O(i,j) = tau*dt/(dx^2*n);
         end
-    end
-    
+    end  
 end 
 
-%S2 = speye(m,m);
-%for i = 1:m
-%    for j = 1:m
- %       if i == j
-  %          S2(i,j) = -2*(tau*dt/(vs*dx^2*n));
-   %     elseif i-1==j
-    %        S2(i,j) = tau*dt/(vs*dx^2*n);
-     %   elseif i+1 ==j
-      %      S2(i,j) = tau*dt/(vs*dx^2*n);
-       % end
-    %end   
-%end 
-%initial condition at x=0, zeta = -2
-z=z0*ones(1,n);
+S2 = speye(m,m);
+for i = 1:m
+   for j = 1:m
+       if i == j
+           S2(i,j) = -2*(tau*dt/(vs*dx^2*n));
+       elseif i-1==j
+           S2(i,j) = tau*dt/(vs*dx^2*n);
+       elseif i+1 ==j
+           S2(i,j) = tau*dt/(vs*dx^2*n);
+       end
+    end   
+end 
+
+
+% solution matrices for h and zeta values
+% create empty matrices
+h_all = zeros(m,n);
+zeta_all = zeros(m,n);
+
+% initial condition for h
+h = 100*ones(1,m); % conditions define h(x,0)
+h = h';
+h_all(:,1) = h;
+
+% initial condition for zeta (assume fresh and salt water separated-->no
+% intrusion = no interface (zeta))
+zeta = zb*ones(1,m); % conditions to define zeta(x,0)
+zeta = zeta';
+zeta_all(:,1) = zeta;
+for t = 2:n
+    hnew = M*h + S1*zeta_all(:,t-1) + (N*dt/S);
+    h_all(:,t) = hnew;
+    
+    zetanew = O*zeta + S2*h_all(:,t-1);
+    zeta_all(:, t) = zetanew;
+
+    h=hnew;
+    zeta = zetanew;
+end
